86d9e82ce568964bd8c18fec6de699e30326172e
Silaswalter <129095844+Silaswalter@users.noreply.github.com>
2025-01-10 13:08:49 -0600

    Update Assignment 1.cpp

--- Assignment 1.cpp
+++ Assignment 1.cpp
@@ -1,18 +1,65 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int findAdd(int number, int array[], int arraySize);
+int findAdd(int number, vector<int> array, int arraySize);
+int checkAmount();
+int checkTarget();
 int main()
 {
     
+    
+    vector<int> array;
+    int amount = checkAmount();
     int number;
-    cout << "Type a number: ";
-    cin >> number;
-    int array[] = {11, 15, 2, 7};
-    findAdd(number, array, sizeof(array)/sizeof(array[0]));
+    cout << "Type the numbers in the array" << endl;
+    for(int i = 0; i < amount; i++)
+    {
+        cin >> number;
+        if(number <= -110 || 110 <= number)
+        {
+            cout << "Invalid number" << endl;
+            i--;
+        }
+        else
+        {
+            array.push_back(number);  
+        }
+
+    }
+    int target = checkTarget();
+    
+    
+    findAdd(target, array, sizeof(array)/sizeof(array[0]));
     return 0;
 }
-
-int findAdd(int number, int array[], int arraySize)
+int checkTarget()
+{
+    int target;
+    cout << "Type a target number: ";
+    cin >> target;
+    if(target <= -110 || 110 <= target)
+    {
+        cout << "Invalid number" << endl;
+        return checkTarget();
+    }
+    else
+    {
+        return target;
+    }
+}
+int checkAmount()
+{
+    int amount;
+    cout << "Type the amount of numbers in the array: ";
+    cin >> amount;
+    if(amount <= 1 || 105 <= amount)
+    {
+        cout << "Invalid number" << endl;
+        return checkAmount();
+    }
+    return amount;
+}
+int findAdd(int number, vector<int> array, int arraySize)
 {
     if(arraySize > 104 || arraySize < 2)
     {
